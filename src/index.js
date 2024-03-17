6bd352809cb6645f822033c048b74771ea0029d7
LotaSchwager <ademir.munoz.r@mail.pucv.cl>
2024-03-16 22:45:17 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const canciones = document.querySelector('.canciones');
+    const listaCanciones = document.querySelector('.lista-canciones');
+    const logo = document.querySelector('logo');
+    
+});
